a20cf65eaee0427e81cf406a7dc65b0267b5f54b
JoseGrass <122506998+JoseGrass@users.noreply.github.com>
2025-10-14 00:48:01 +0000

    styles commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,42 @@
-import { BrowserRouter as Router, Route, Routes, Link, Navigate } from 'react-router-dom';
-import { useState } from 'react'
-import './App.css'
-import Home from './Home'
-import Informativa from './Informativa'
-import Favoritos from './Favoritos'
-import Original from './Original'
-import Detalle from './Detalle'
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import './App.css';
+import Home from './Home';
+import Informativa from './Informativa';
+import Favoritos from './Favoritos';
+import Original from './Original';
+import Detalle from './Detalle';
 
 function App() {
- 
   return (
-    <>
-      <Router>
-
+    <Router>
+      {/* Menú fijo */}
+      <header className="menu-fixed">
         <nav className="c-menu">
-          <Link to="/">Home</Link>
-          <Link to="/Informativa">Informativa</Link>
-          <Link to="/Original">Original</Link>
-          <Link to="/Favoritos">Favoritos</Link>
+          <div className="menu-logo">📍ColombiaData</div>
+          <input type="checkbox" id="menu-toggle" className="menu-toggle" />
+          <label htmlFor="menu-toggle" className="menu-icon">☰</label>
+
+          <div className="menu-links">
+            <Link to="/">Home</Link>
+            <Link to="/Informativa">Informativa</Link>
+            <Link to="/Original">Original</Link>
+            <Link to="/Favoritos">Favoritos</Link>
+          </div>
         </nav>
+      </header>
 
-
-      <Routes>
-          <Route path="/" element={<Home /> } />
-          <Route path="/Informativa" element={<Informativa /> } />
-          <Route path="/original" element={<Original /> } />
-          <Route path="/Favoritos" element={<Favoritos /> } />
-          <Route path="/Detalle/:depto/:municipio" element={<Detalle /> } />
-      </Routes>
+      {/* Contenido de la app */}
+      <main className="main-content">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/Informativa" element={<Informativa />} />
+          <Route path="/Original" element={<Original />} />
+          <Route path="/Favoritos" element={<Favoritos />} />
+          <Route path="/Detalle/:depto/:municipio" element={<Detalle />} />
+        </Routes>
+      </main>
     </Router>
-      
-    </>
-  )
+  );
 }
 
-export default App
+export default App;
